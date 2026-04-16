208bc55057a5b541c4c98b4612c4a91bf45e0764
jihanejador <jihanejador@gmail.com>
2026-04-16 12:22:42 +0100

    insert les tables

--- script.sql
+++ script.sql
@@ -44,3 +44,32 @@ use management;
     FOREIGN KEY (id_courses) REFERENCES courses(id)
  );
  ALTER TABLE courses CHANGE id_users id_prof INT ;
+ INSERT INTO roles(label) 
+ VALUES ("admin"),
+ ("prof"),
+ ("student");
+
+INSERT INTO users(fristname ,lastname,email,pswrd ) VALUES
+("jihane","jador","jihane@gmail.com","123456"),
+("sara","allaoui","sara@gmail.com","123457"),
+("ayoub","banani","ayoub@gmail.com","123458");
+
+INSERT INTO classes( name,classrom_number) VALUES
+('Dev Web 2026', 101),
+('Data 2026', 102),
+('java web',103);
+
+INSERT INTO courses(title,description,total_hours,id_prof) VALUES
+('JavaScript', 'JS Basics', 40, 2),
+('Database', 'SQL Course', 30, 2),
+('php','php course',50,1);
+
+INSERT INTO students(dateofbrith,student_number,id_users,id_classes) VALUES 
+('2000-05-10', 12345, 3, 1),
+('2003-01-13', 32456, 2, 1),
+('2005-03-22', 23987, 4, 1);
+
+INSERT INTO enrollements(daate,status,id_students,id_courses) VALUES
+('2026-01-01', 'Actif', 1, 1),
+('2026-01-02', 'Actif', 1, 2),
+('2026-01-05', 'Actif', 2, 3);
